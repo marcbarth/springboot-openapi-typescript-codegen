f4513995bd3a3bcff9cac637ac46b90d9dd3ce3c
Marc Barth <marc.barth@gmail.com>
2023-02-28 00:30:29 +0100

    spring boot upgrade

--- src/main/kotlin/com/example/springbootopenapitypescriptcodegen/swagger/OpenApiConfigurationProperties.kt
+++ src/main/kotlin/com/example/springbootopenapitypescriptcodegen/swagger/OpenApiConfigurationProperties.kt
@@ -1,9 +1,9 @@
 package com.example.springbootopenapitypescriptcodegen.swagger
 
+import jakarta.validation.constraints.NotBlank
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import org.springframework.validation.annotation.Validated
-import javax.validation.constraints.NotBlank
 
 
 @Validated
